bdc0c9cdd0d1c8891faf8d8efffb636ea3e37d73
ELITEBOOK <ELITEBOOK@DESKTOP-7MJDDPC>
2022-11-22 03:33:47 +0100

    cercle

--- Program.cs
+++ Program.cs
@@ -26,3 +26,52 @@
         Console.Out.WriteLine("POINT(" + x + "," + y + ")");
     }
 }
+class Cercle : Point
+{
+    private Point centre;                   // Attribut de type Point
+    private double rayon;
+
+    public Point Centre
+    {
+        get { return centre; }
+        set { centre = value; }
+    }
+
+    public double Rayon
+    {
+        get { return rayon; }
+        set { rayon = value; }
+    }
+
+    public Cercle(Point p, double r)
+    {
+        centre = p;
+        rayon = r;
+    }
+
+    public double getPerimetre()
+    {
+        return Math.Round(2 * Math.PI * rayon, 2);            //Arrondir le résultat à 2 chiffres aprés la virgule.
+    }
+
+    public double getSurface()
+    {
+        return Math.Round(Math.PI * rayon * rayon, 2);
+    }
+
+    public bool Appartient(Point p)
+    {
+        double dis;
+        dis = Math.Sqrt(Math.Pow(centre.X - p.X, 2) + Math.Pow(centre.Y - p.Y, 2));
+        //La distance entre le centre du cercle courant et le point passé en paramètre.
+        if (dis <= rayon)
+            return true;
+        else
+            return false;
+    }
+
+    public void Afficher()
+    {
+        Console.Out.WriteLine("CERCLE(" + centre.X + "," + centre.Y + "," + rayon + ")");
+    }
+}
