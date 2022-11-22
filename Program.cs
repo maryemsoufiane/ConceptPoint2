995867159a63c93f78b9654b97994cf208fed453
ELITEBOOK <ELITEBOOK@DESKTOP-7MJDDPC>
2022-11-22 03:24:50 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿public class Point
+{
+    private double x;
+    private double y;
+
+    public double X
+    {
+        get { return x; }
+        set { x = value; }
+    }
+
+    public double Y
+    {
+        get { return y; }
+        set { y = value; }
+    }
+
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void Afficher()
+    {
+        Console.Out.WriteLine("POINT(" + x + "," + y + ")");
+    }
+}
